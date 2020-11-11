f327e4d1bfb38b9931eba4c1e0ec445e9fe2a9a4
DravitKumar <dravitkumar@vm.mshome.net>
2020-11-11 21:33:16 +0530

    redux thunk get request complete

--- src/components/UserContainer.js
+++ src/components/UserContainer.js
@@ -3,13 +3,22 @@ import { useSelector, useDispatch } from "react-redux";
 import { FetchUsers } from "../redux";
 
 function UserContainer() {
+  const userData = useSelector((state) => state.user);
   const dispatch = useDispatch();
-  const userdata = useSelector((state) => state.user);
 
   useEffect(() => {
     dispatch(FetchUsers());
-  }, []);
-  return <div>{userdata.loading}</div>;
+  }, [dispatch]);
+  return userData.loading ? (
+    <h2>Loading...</h2>
+  ) : userData.error ? (
+    <h2>{userData.error}</h2>
+  ) : (
+    <div>
+      <h2>User List</h2>
+      <div>{userData && userData.data && userData.data.map(user => <p key={user.id}>{user.name}</p>)}</div>
+    </div>
+  );
 }
 
 export default UserContainer;
--- src/redux/user/UserActions.js
+++ src/redux/user/UserActions.js
@@ -24,8 +24,8 @@ export const FetchUsersError = (error) => {
 // Create Action Creator 
 export const FetchUsers = () => {
   return async (dispatch, getState) => {
-    const statebefore = getState();
-    console.log(statebefore);
+    // const statebefore = getState();
+    // console.log(statebefore);
     dispatch(FetchUserRequest);
     axios.get('https://jsonplaceholder.typicode.com/users')
     .then(response => {
